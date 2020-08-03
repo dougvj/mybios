182dee8b2a73efb77941721eaeeb65ec0f7b5824
Doug Johnson <dougvj@gmail.com>
2020-08-03 01:33:38 -0600

    Add simple memory test routine

--- reset.asm
+++ reset.asm
@@ -1,35 +1,80 @@
 BITS 16
 CPU 486
-%macro PCODE 1
-push ax
-mov al, %1
-out 0x80, al
-pop ax
-%endmacro
-
+%include "pcode.asm"
 extern wait_forever
 extern fill_dummy_ivt
 extern init_video
 extern main
 extern enter_protected_mode
 
+
+%macro WAIT 1
+    push ax
+    mov ax, %1
+    %%do_nop:
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        dec ax
+        jnz %%do_nop
+    pop ax
+%endmacro
+
+%macro DUMP_BYTE 3
+PCODE %1
+WAIT 0xFFFF
+PCODE %2
+WAIT 0xFFFF
+PCODE %1
+WAIT 0xFFFF
+PCODE %3
+WAIT 0xFFFF
+%endmacro
+
 SECTION .real_mode_text
+
 entry_point:
-    cli
     PCODE 0x00
-    call wait_forever
-    call wait_forever
-    call wait_forever
+    cli
     PCODE 0x01
+    ;call wait_forever
+    ;call wait_forever
+    ;call wait_forever
     ;Wait for ram stabilitiy if ever
-    mov ax, 0x0040
-    mov ds, ax
-    mov ax, 0xaaaa
-    mov [ds:0x0000], ax
-    mov bx, [ds:0x0000]
-    cmp ax, bx
+    mov si, 0x0000
+    mov cx, 0x0000
+    mov ds, cx
+test_byte:
+    mov cx, si
+    mov al, 0xAA
+    mov [ds:si], al
+    mov bl, [ds:si]
+    cmp al, bl
     ;If ram isn't stable, keep trying
-    jne entry_point
+    jne test_fail
+    mov al, 0x55
+    mov [ds:si], al
+    mov bl, [ds:si]
+    cmp al, bl
+    jne test_fail 
+    inc si
+    jz init_runtime_16
+    jmp test_byte
+test_fail:
+    mov cx, si
+    WAIT 0xFFF
+    DUMP_BYTE 0xFF, ch, cl
+    DUMP_BYTE 0xFD, ah, al
+    DUMP_BYTE 0xFE, bh, bl
+    WAIT 0xFFF
+    jmp test_fail
+init_runtime_16:
+    PCODE 0x02
     mov ax, 0x0000
     mov ss, ax
     mov bp, 0x400
@@ -43,10 +88,14 @@ entry_point:
 SECTION .reset
 BITS 16
 global reset_vector
-jmp dword entry_point
+PCODE 0xCA
+jmp dword init_runtime_16
 
 SECTION .enter_protected_mode
 BITS 16
 incbin "enter_protected_mode.bin"
 BITS 32
+PCODE 0x10
 call main
+SECTION .signature
+dd 0xDEADBEEF
